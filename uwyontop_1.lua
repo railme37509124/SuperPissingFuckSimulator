b998b30ae80f0393f8bee4950acde15f7dbba6d7
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-04-24 22:54:44 +0100

    Create uwyontop_1.lua

---
+++ uwyontop_1.lua
@@ -0,0 +1,34 @@
+--uwy on top of u skids tryna read the code LOL
+function increaseX(x: String, t: Number)
+    for i = 1, t do
+        game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Multiplier"):InvokeServer(unpack({
+            [1] = x,
+            [2] = 0.9
+        }))
+    end
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Made by uwy (@uwy5)";
+	Text = "Want more scripts? discord.gg/FJQXYfaAh2 (copied to clipboard)";
+	Duration = 5;
+})
+local suc, res = pcall(function() setclipboard(https://discord.gg/FJQXYfaAh2) end)
+if res and not suc then
+ game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Setclipboard issue";
+	Text = "Failed to copy \"discord.gg/FJQXYfaAh2\" to clipboard";
+	Duration = 5;
+ })
+end
+
+task.spawn(function()
+    while task.wait() do
+        if getgenv().luckMultiplier then
+            increaseX("LuckMultiplier", getgenv().speed_MAY_INCREASE_LAG_IF_TOO_HIGH)
+        end
+        if getgenv().chestSpeed then
+            increaseX("ChestSpeedMultiplier", getgenv().speed_MAY_INCREASE_LAG_IF_TOO_HIGH)
+        end
+    end
+end)
