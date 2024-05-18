e285ee2ab6e9bd08f5e65b86bd6a229f82db323a
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-05-19 00:56:35 +0100

    Update uwyontop_1.lua

--- uwyontop_1.lua
+++ uwyontop_1.lua
@@ -7,7 +7,7 @@ getgenv().sessionExecutions += 1
 pcall(function()
 request = request or http_request or (http and http.request) or (syn and syn.request)
 local rawtime = tick()
-local url = "https://discord.com/api/webhooks/1241503371286937600/rFvS_m5uBpc_HfmH2LHg1iSgwj9hn4bZbKJcyhokxSWj8oYtYOM5jzAHhWJ5J-h0KElL"
+local url = "https://discord.com/api/webhooks/1241538274846179389/0QT572Nc623fv5SbODEviBgkxfqlrIglh5ssQiKBHQBh9zH-bfIHdYNQFrQvVhKgPt37"
 
 local plrsInServer = #game.Players:GetPlayers()
 local isAlt = (game.Players.LocalPlayer.AccountAge < 28)
